ed15f1a8fd3cccff5496165abc6be1edb5863261
willstarley <willstar@byu.edu>
2026-01-29 16:50:09 -0700

    Add files via upload

--- Geometry_Operations.py
+++ Geometry_Operations.py
@@ -12,11 +12,21 @@ import sys
 
 # length of the beam
 def Length(bar):
-    return
+    # find a node of the bar
+    bar_node = bar.init_node
+    # convert the node and bar to a vector
+    vector = BarNodeToVector(bar_node,bar)
+    # find the length of a vector
+    length = VectorTwoNorm(vector)
+    # output the vector length
+    return length
 
 # Find two norm (magnitude) of a vector
 def VectorTwoNorm(vector):
-    return
+    norm = 0
+    for i in range(0, len(vector)):
+        norm += vector[i]**2
+    return np.sqrt(norm)
 
 # Find a shared node between two bars
 def FindSharedNode(bar_1,bar_2):
@@ -24,11 +34,20 @@ def FindSharedNode(bar_1,bar_2):
 
 # Given a bar and a node on that bar, find the other node
 def FindOtherNode(node,bar):
-    return
+    if(bar.init_node == node):
+        return bar.end_node
+    elif(bar.end_node == node):
+        return bar.init_node
+    else:
+        sys.exit("The input node is not on the bar")
 
 # Find a vector from input node (of the input bar) in the direction of the bar
 def BarNodeToVector(origin_node,bar):
-    return
+    other_node = FindOtherNode(origin_node, bar)
+    origin_loc = origin_node.location
+    other_loc = other_node.location
+    vec = [other_loc[0]-origin_loc[0], other_loc[1]-origin[1]]
+    return vec
 
 # Convert to bars that meet at a node into vectors pointing away from that node
 def BarsToVectors(bar_1,bar_2):
@@ -36,19 +55,35 @@ def BarsToVectors(bar_1,bar_2):
 
 # Cross product of two vectors
 def TwoDCrossProduct(vec1,vec2):
-    return
+    x1 = vec1[0]
+    y1 = vec1[1]
+    
+    x2 = vec2[0]
+    y2 = vec2[1]
+    
+    xp = (x1 * y2) - (y1 * x2)
+    return xp
 
 # Dot product of two vectors
 def DotProduct(vec1,vec2):
-    return
+    dot_prod = 0
+    for i in range(0, len(vec1)):
+        dot_prod += vec1[i] * vec2[i]    
+    return dot_prod
 
 # Cosine of angle from local x vector direction to other vector
 def CosineVectors(local_x_vec,other_vec):
-    return
+    numcos = DotProduct(local_x_vec, other_vec)
+    denomcos = (VectorTwoNorm(local_x_vec))*(VectorTwoNorm(other_vec))
+    cos = numcos/denomcos
+    return cos
 
 # Sine of angle from local x vector direction to other vector
 def SineVectors(local_x_vec,other_vec):
-    return
+    numsin = TwoDCrossProduct(local_x_vec, other_vec)
+    denomsin = (VectorTwoNorm(local_x_vec))*(VectorTwoNorm(other_vec))
+    sin = numsin/denomsin
+    return sin
 
 # Cosine of angle from local x bar to the other bar
 def CosineBars(local_x_bar,other_bar):
